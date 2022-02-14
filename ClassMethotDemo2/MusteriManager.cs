91a51cc12ada1782529ae49193fddb5339c3cc4f
gurogluefekan@gmail.com <gurogluefekan@gmail.com>
2022-02-14 19:23:21 +0300

    Add project files.

---
+++ ClassMethotDemo2/MusteriManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethotDemo2
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Hoşgeldiniz Sayın: " + musteri.ısım);
+        }
+        public void list(Musteri[] musteri)
+        {
+            Console.WriteLine("Listemleme Başarılı ");
+            foreach (Musteri x in musteri)
+            {
+                Console.WriteLine(x.ID);
+            }
+        }   
+        public void Del(Musteri musteri)
+        {
+            Console.WriteLine("Silme İŞlemi Başarılı " + musteri.ısım);
+        }
+        
+	
+    }
+}
---
+++ ClassMethotDemo2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassMethotDemo2
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 15495926;
+            musteri1.ısım = "Samet";
+            musteri1.soyısım = "Ulaşan";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 169559;
+            musteri2.ısım = "Efe";
+            musteri2.soyısım = "Güroğlu";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.ID = 656523;
+            musteri3.ısım = "Ahmet";
+            musteri3.soyısım = "Kargan";
+
+            Musteri[] musteri = new Musteri[] { musteri1,musteri2, musteri3};
+
+            MusteriManager musteriManagers = new MusteriManager();
+            musteriManagers.Add(musteri1);
+            musteriManagers.Add(musteri2);
+            musteriManagers.Add(musteri3);
+            musteriManagers.list(musteri);
+            musteriManagers.Del(musteri1);
+            musteriManagers.Del(musteri2);
+            musteriManagers.Del(musteri3);
+
+
+
+        }
+    }
+}
